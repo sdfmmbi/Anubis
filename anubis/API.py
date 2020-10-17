a8a92289b7fbceb558cc794a5789a633493774dd
sdfmmbi <70980276+sdfmmbi@users.noreply.github.com>
2020-10-17 06:57:10 -0700

    Update API.py

--- anubis/API.py
+++ anubis/API.py
@@ -1,4 +1,4 @@
-SHODAN_KEY = "PGHgSzfOUl5UJ44qJ53f7tZn9gQwvwIM"
+SHODAN_KEY = "lRhBrwJmn5b7pP0jtr3GOdyTLsJXI9z6"
 # Public Shodan key bundled with Anubis
-CENSYS_ID = ""
-CENSYS_SECRET = ""
+CENSYS_UID = '6f5c0df4-3908-430f-9f17-fb350db03fa3'
+CENSYS_SECRET = '9s7nwqeXjJxzBkij4gxFNYC5EQYhX6QG'
